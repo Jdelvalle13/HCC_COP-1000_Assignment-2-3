deb916299a8070c4eebadba6543ef6c82c310b8c
Jaime Del Valle <jdelvalle13@hawkmail.hccfl.edu>
2024-09-13 18:17:36 -0400

    Modified code.  Added profit,  SalePrice, and saleProfit

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ wholesalePrice = 200.00
 
 # Write your assignment statements here for profit, salePrice, and saleProfit
 
+# Calculate (profit) as the retail price minus the wholesale price
+profit = retailPrice - wholesalePrice
+
+# Calculate (salePrice) as 25 percent deducted from the retail price
+salePrice = retailPrice - (retailPrice * .25)
+
+# Calculate the profit when the sale price is used (saleProfit) as the sale price minus the wholesale price
+saleProfit = salePrice - wholesalePrice
+
 print("Item Name: " + itemName)
 print("Retail Price: $" + str(retailPrice))
 print("Wholesale Price: $" + str(wholesalePrice))
